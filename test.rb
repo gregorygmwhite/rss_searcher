be2776266e7cd31b327d75f2ec2f454a2642be23
Gregory White <ideation200@me.com>
2015-05-02 01:55:35 -0700

    adds rspec testing

--- test.rb
+++ test.rb
@@ -1,10 +1,34 @@
 require_relative 'rss_searcher.rb'
-require 'pp'
+require 'rspec'
 
-keywords = ["Vietnam", "China", "Money"]
-feed_filname = "rss_feeds.txt"
-match_level = "ONE_OR_MORE"
-last_three_days = Date.today - 3
+RSpec.describe "Searcher" do
+  context "Different Match Levels" do
+    it "should give back links for 'one or more' match level" do
+      keywords = ["the", "asdfasdfasdfasdfnn;jiuiwe"]
+      feed_filname = "rss_feeds.txt"
+      match_level = "ONE_OR_MORE"
+      last_three_days = Date.today - 3
+      searcher = RSS_Searcher.new(feed_filname, keywords,last_three_days)
+      expect(searcher.get_links_of_interest(match_level).length > 0)
+    end
 
-searcher = RSS_Searcher.new(feed_filname, keywords,last_three_days)
-pp searcher.get_links_of_interest(match_level)
+    it "should give no back links for 'all' match level with weird string in keywords" do
+      keywords = ["the", "asdfasdfasdfasdfnn;jiuiwe"]
+      feed_filname = "rss_feeds.txt"
+      match_level = "ALL"
+      last_three_days = Date.today - 3
+      searcher = RSS_Searcher.new(feed_filname, keywords,last_three_days)
+      expect(searcher.get_links_of_interest(match_level).length === 0)
+    end
+
+    it "should give some back links for '2%' match level" do
+      keywords = ["the", "asdfasdfasdfasdfnn;jiuiwe"]
+      feed_filname = "rss_feeds.txt"
+      match_level = 2
+      last_three_days = Date.today - 3
+      searcher = RSS_Searcher.new(feed_filname, keywords,last_three_days)
+      expect(searcher.get_links_of_interest(match_level).length > 0)
+    end
+
+  end
+end
