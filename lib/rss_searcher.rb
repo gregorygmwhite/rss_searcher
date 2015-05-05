bc0cddfce7a09ede0d22756216b03a774e759b89
Gregory White <ideation200@me.com>
2015-05-04 21:43:50 -0700

    bug fix which params are passed to searcher

--- lib/rss_searcher.rb
+++ lib/rss_searcher.rb
@@ -4,7 +4,7 @@ require_relative "file_reader.rb"
 class RSS_Searcher
 
   def get_links_of_interest(feeds, keywords, match_level="ONE_OR_MORE", minimum_publish_date=Date.today)
-    self.set_relevant_params()
+    self.set_relevant_params(feeds, keywords, minimum_publish_date)
     return Array.new unless @feed_urls && @keywords && @minimum_publish_date
     links_of_interest = Array.new
     begin
@@ -17,7 +17,7 @@ class RSS_Searcher
     return links_of_interest
   end
 
-  def set_relevant_params(feeds, keywords, minimum_publish_date=Date.today)
+  def set_relevant_params(feeds, keywords, minimum_publish_date)
     @feed_urls = File_Reader::get_lines(feeds)
     @keywords = File_Reader::get_lines(keywords)
     @minimum_publish_date = minimum_publish_date
