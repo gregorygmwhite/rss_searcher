493dabc0b77c93de6d2f604155c14fe811b029a2
Gregory White <ideation200@me.com>
2015-05-04 21:40:16 -0700

    change gem interface for more flexibility

--- lib/rss_searcher.rb
+++ lib/rss_searcher.rb
@@ -3,13 +3,8 @@ require_relative "file_reader.rb"
 
 class RSS_Searcher
 
-  def initialize(feeds, keywords, minimum_publish_date=Date.today)
-    @feed_urls = File_Reader::get_lines(feeds)
-    @keywords = File_Reader::get_lines(keywords)
-    @minimum_publish_date = minimum_publish_date
-  end
-
-  def get_links_of_interest(match_level="ONE_OR_MORE")
+  def get_links_of_interest(feeds, keywords, match_level="ONE_OR_MORE", minimum_publish_date=Date.today)
+    self.set_relevant_params()
     return Array.new unless @feed_urls && @keywords && @minimum_publish_date
     links_of_interest = Array.new
     begin
@@ -20,6 +15,12 @@ class RSS_Searcher
       links_of_interest = Array.new
     end
     return links_of_interest
+  end
+
+  def set_relevant_params(feeds, keywords, minimum_publish_date=Date.today)
+    @feed_urls = File_Reader::get_lines(feeds)
+    @keywords = File_Reader::get_lines(keywords)
+    @minimum_publish_date = minimum_publish_date
   end
 
 end
